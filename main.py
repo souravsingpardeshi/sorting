f8ec0c2bd901b00274130058ce90dec47d7d898c
Souravsing Pardeshi <63045639+souravsingpardeshi@users.noreply.github.com>
2021-02-22 15:36:34 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,20 @@
+import matplotlib.pyplot as plt
+from matplotlib.animation import FuncAnimation
+import matplotlib as mp
+mp.use('TkAgg')
+import numpy as np
+import random
+import streamlit as st
+from sortviz import *
+def local_css(file_name):
+    with open(file_name) as f:
+        st.markdown(f'<style>{f.read()}</style>',unsafe_allow_html=True)
+local_css("style.css")  
+# set the style of the graph
+st.title("Data structure buddy")
+alg = ["home","sorting"]
+choice = st.sidebar.selectbox("select choice",alg)
+if choice == "Home":
+    st.write("""some shit goes here""")
+elif choice == "sorting":
+    sort()
