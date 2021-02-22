d561e94496bae6d41681d8e50687a7a337fa3428
Souravsing Pardeshi <63045639+souravsingpardeshi@users.noreply.github.com>
2021-02-22 15:48:26 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
-import matplotlib as mp
-mp.use('TkAgg')
+matploatlib.use('TkAgg')
 import numpy as np
 import random
 import streamlit as st
